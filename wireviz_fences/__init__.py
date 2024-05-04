6ff3ecc8c13bfa1e0b98bad354178aac8c319bbd
Christopher Petrilli <petrilli@amber.org>
2024-05-03 22:51:42 -0700

    Updated to strip off unnecessary XML cruft

--- wireviz_fences/__init__.py
+++ wireviz_fences/__init__.py
@@ -1,12 +1,24 @@
 """Top-level package for Wireviz Fences."""
+import re
 from wireviz import wireviz
 
 __author__ = """Christopher Petrilli"""
 __email__ = 'petrilli@amber.org'
 __version__ = '0.1.0'
 
-
 def fence_wireviz(source, language, css_class, options, md, classes=None, id_value='',
                   attrs=None, **kwargs):
     results = wireviz.parse(source, return_types="svg")
-    return results.decode("utf-8")
+
+    # Turn it into one long string
+    raw = results.decode("utf-8")
+    raw = raw.replace("\n", "")
+
+    # Strip off the XML header nonsense because we're going to embed in
+    # HTML
+    try:
+        svg_only = re.match(r""".*(\<svg.*)""", raw)[1]
+        return svg_only
+    except:
+        return "FAILED_PARSE"
+    
